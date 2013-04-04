b69147763eb7fffbef3f4f5ad0155a37bf2f259f
Martin Preisler <mpreisle@redhat.com>
2013-04-04 17:58:10 +0200

    Added OscapCapabilities class, it parses output of 'oscap --v'
    This will be used to figure out if we can or cannot send source
    datastreams, receive ARFs, use SCE, etc...

---
+++ include/OscapCapabilities.h
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2013 Red Hat Inc., Durham, North Carolina.
+ * All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *      Martin Preisler <mpreisle@redhat.com>
+ */
+
+#ifndef SCAP_WORKBENCH_OSCAP_SCANNER_CAPABILITIES_H_
+#define SCAP_WORKBENCH_OSCAP_SCANNER_CAPABILITIES_H_
+
+#include "ForwardDecls.h"
+
+#include <QString>
+
+/**
+ * @brief Figures out oscap capabilities based on version and compile flags
+ */
+class OscapCapabilities
+{
+    public:
+        OscapCapabilities();
+
+        /**
+         * @brief Resets all stored capabilities to unknown or not supported
+         *
+         * @see OscapCapabilities::parse
+         */
+        void clear();
+
+        /**
+         * @brief Parses output of 'oscap --v' and interprets the results
+         *
+         * The results are stored in this class. All previously stored results
+         * will be lost!
+         *
+         * @param mmv Verbatim output of 'oscap --v' to be processed
+         */
+        void parse(const QString& mmv);
+
+        /**
+         * @brief Returns true if --progress flag is supported
+         *
+         * If the flag is not supported, we don't do any GUI progress reporting.
+         */
+        bool progressReporting() const;
+
+        /**
+         * @brief Returns true if source datastreams are supported
+         */
+        bool sourceDatastreams() const;
+
+        /**
+         * @brief Returns true if ARFs are supported
+         */
+        bool ARF() const;
+
+        const QString& XCCDFVersion() const;
+        const QString& OVALVersion() const;
+        const QString& CPEVersion() const;
+
+    private:
+        QString mVersion;
+
+        bool mProgressReporting;
+        bool mRemediationProgressReporting;
+        bool mSourceDataStreams;
+        bool mARF;
+        bool mSCE;
+
+        QString mXCCDFVersion;
+        QString mOVALVersion;
+        QString mCPEVersion;
+        QString mSCEVersion;
+};
+
+#endif
---
+++ src/OscapCapabilities.cpp
@@ -0,0 +1,150 @@
+/*
+ * Copyright 2013 Red Hat Inc., Durham, North Carolina.
+ * All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *      Martin Preisler <mpreisle@redhat.com>
+ */
+
+#include "OscapCapabilities.h"
+
+#include <QRegExp>
+#include <QStringList>
+
+OscapCapabilities::OscapCapabilities()
+{
+    clear();
+}
+
+void OscapCapabilities::clear()
+{
+    mVersion = "Unknown";
+
+    mProgressReporting = false;
+    mSourceDataStreams = false;
+    mARF = false;
+    mSCE = false;
+
+    mXCCDFVersion = "Unknown";
+    mOVALVersion = "Unknown";
+    mCPEVersion = "Unknown";
+    mSCEVersion = "Unknown";
+}
+
+static bool versionGreaterThan(const QString& a, const QString& b)
+{
+    const QStringList aSplit = a.split(".");
+    const QStringList bSplit = a.split(".");
+
+    size_t pos = 0;
+
+    while (pos < aSplit.size() && pos < bSplit.size())
+    {
+        if (aSplit[pos].toInt() < bSplit[pos].toInt())
+            return false;
+
+        ++pos;
+    }
+
+    return aSplit.size() > 0 && bSplit.size() > 0;
+}
+
+void OscapCapabilities::parse(const QString& mmv)
+{
+    clear();
+
+    const QStringList lines = mmv.split("\n");
+
+    if (lines.size() < 1)
+        return; // TODO: Throw exception?
+
+    const QStringList firstLine = lines[0].split(" ", QString::SkipEmptyParts);
+    const QString& versionCandidate = firstLine.last();
+
+    if (!versionCandidate.contains(QRegExp("^([0-9]+\\.){2,}[0-9]+$")))
+        return; // TODO: Throw exception?
+
+    mVersion = versionCandidate;
+
+    if (versionGreaterThan(mVersion, "0.9.3"))
+        mProgressReporting = true;
+
+    if (versionGreaterThan(mVersion, "0.9.5"))
+        mRemediationProgressReporting = true;
+
+    if (versionGreaterThan(mVersion, "0.9.0"))
+        mSourceDataStreams = true;
+
+    if (lines.size() < 4 || !lines[3].contains("Supported specifications"))
+        return; // TODO: Throw exception?
+
+    size_t linePos = 4;
+    while (linePos < lines.size())
+    {
+        const QStringList versionLineSplit = lines[linePos].split(": ");
+
+        if (versionLineSplit[0] == "XCCDF Version")
+            mXCCDFVersion = versionLineSplit[1];
+        else if (versionLineSplit[0] == "OVAL Version")
+            mOVALVersion = versionLineSplit[1];
+        else if (versionLineSplit[0] == "CPE Version")
+            mCPEVersion = versionLineSplit[1];
+        else if (versionLineSplit[0] == "Script check engine")
+        {
+            mSCE = true;
+            mSCEVersion = versionLineSplit[1];
+        }
+        else
+        {
+            // TODO: Warn about unknown version
+        }
+
+        if (lines[linePos] == "")
+            break; // End of the list
+
+        ++linePos;
+    }
+}
+
+bool OscapCapabilities::progressReporting() const
+{
+    return mProgressReporting;
+}
+
+bool OscapCapabilities::sourceDatastreams() const
+{
+    return mSourceDataStreams;
+}
+
+bool OscapCapabilities::ARF() const
+{
+    return mARF;
+}
+
+const QString& OscapCapabilities::XCCDFVersion() const
+{
+    return mXCCDFVersion;
+}
+
+const QString& OscapCapabilities::OVALVersion() const
+{
+    return mOVALVersion;
+}
+
+const QString& OscapCapabilities::CPEVersion() const
+{
+    return mCPEVersion;
+}
