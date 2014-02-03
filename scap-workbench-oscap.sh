6affd52c5c08fc1257cd2dd9bf10c434744244f5
Martin Preisler <mpreisle@redhat.com>
2014-02-03 15:05:33 +0100

    Do not copy using sudo but instead copy and then chown after evaluation is completed
    sudo may or may not require tty depending on the configuration.

--- scap-workbench-oscap.sh
+++ scap-workbench-oscap.sh
@@ -84,24 +84,23 @@ RET=$?
 
 popd > /dev/null
 
-chown -R $wrapper_uid:$wrapper_gid "$TEMP_DIR"
-
-function sudo_copy
+function chown_copy
 {
     local what=$1
     local where=$2
 
     # sudo only required if wrapper_{uid,gid} differs from real_{uid,gid}
     if [ $wrapper_uid -ne $real_uid ] || [ $wrapper_gid -ne $real_gid ]; then
-        sudo -u \#$wrapper_uid -g \#$wrapper_gid [ ! -f "$what" ] || cp "$what" "$where"
+        [ ! -f "$what" ] || cp "$what" "$where"
+        chown $wrapper_uid:$wrapper_gid $where
     else
         [ ! -f "$what" ] || cp "$what" "$where"
     fi
 }
 
-sudo_copy "$TEMP_DIR/results-xccdf.xml" $TARGET_RESULTS_XCCDF
-sudo_copy "$TEMP_DIR/results-arf.xml" $TARGET_RESULTS_ARF
-sudo_copy "$TEMP_DIR/report.html" $TARGET_REPORT
+chown_copy "$TEMP_DIR/results-xccdf.xml" $TARGET_RESULTS_XCCDF
+chown_copy "$TEMP_DIR/results-arf.xml" $TARGET_RESULTS_ARF
+chown_copy "$TEMP_DIR/report.html" $TARGET_REPORT
 
 rm -r "$TEMP_DIR"
 
