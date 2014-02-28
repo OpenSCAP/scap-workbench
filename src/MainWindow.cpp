b4a7761e33aadc22140abcd0cdf94a44143b70bc
Martin Preisler <mpreisle@redhat.com>
2014-02-28 13:00:12 +0100

    Provide helpful rule result tooltips explaining the meaning

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -933,17 +933,64 @@ void MainWindow::scanProgressReport(const QString& rule_id, const QString& resul
     const QString preferredTitle = oscapTextIteratorGetPreferred(xccdf_item_get_title(item));
     const QString preferredDesc = oscapTextIteratorGetPreferred(xccdf_item_get_description(item));
 
+    QString resultTooltip;
     QBrush resultBrush;
     if (result == "processing")
+    {
         resultBrush.setColor(Qt::darkYellow);
+        resultTooltip = "This rule is currently being processed.";
+    }
     else if (result == "pass")
+    {
         resultBrush.setColor(Qt::darkGreen);
-    else if (result == "fixed")
-        resultBrush.setColor(Qt::darkGreen);
+        resultTooltip = "The target system or system component satisfied all the conditions of this rule.";
+    }
     else if (result == "fail")
+    {
         resultBrush.setColor(Qt::red);
+        resultTooltip = "The target system or system component did not satisfy every condition of this rule.";
+    }
     else if (result == "error")
+    {
         resultBrush.setColor(Qt::red);
+        resultTooltip = "The checking engine could not complete the evaluation, therefore the status of the target's "
+                "compliance with the rule is not certain. This could happen, for example, if a testing "
+                "tool was run with insufficient privileges and could not gather all of the necessary information.";
+    }
+    else if (result == "unknown")
+    {
+        resultBrush.setColor(Qt::darkGray);
+        resultTooltip = "The testing tool encountered some problem and the result is unknown.";
+    }
+    else if (result == "notapplicable")
+    {
+        resultBrush.setColor(Qt::darkGray);
+        resultTooltip = "The rule was not applicable to the target machine of the test. For example, the "
+                "rule might have been specific to a different version of the target OS, or it might "
+                "have been a test against a platform feature that was not installed.";
+    }
+    else if (result == "notchecked")
+    {
+        resultBrush.setColor(Qt::darkGray);
+        resultTooltip = "The rule was not evaluated by the checking engine. There were no check elements "
+                "inside the rule or none of the check systems of the check elements were supported.";
+    }
+    else if (result == "notselected")
+    {
+        resultBrush.setColor(Qt::darkGray);
+        resultTooltip = "The rule was not selected in the benchmark.";
+    }
+    else if (result == "informational")
+    {
+        resultBrush.setColor(Qt::darkGray);
+        resultTooltip = "The rule was checked, but the output from the checking engine is simply "
+                "information for auditors or administrators; it is not a compliance category.";
+    }
+    else if (result == "fixed")
+    {
+        resultBrush.setColor(Qt::darkGreen);
+        resultTooltip = "The rule had failed, but was then fixed (most probably using remediation).";
+    }
     else
         resultBrush.setColor(Qt::darkGray);
 
@@ -959,6 +1006,7 @@ void MainWindow::scanProgressReport(const QString& rule_id, const QString& resul
     treeItem->setText(0, preferredTitle);
     treeItem->setToolTip(0, preferredDesc);
     treeItem->setText(1, result);
+    treeItem->setToolTip(1, resultTooltip);
 
     treeItem->setForeground(1, resultBrush);
 
