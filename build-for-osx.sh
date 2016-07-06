00608e133162f9e6d92da623ca5a27237086ee74
Martin Preisler <mpreisle@redhat.com>
2016-07-06 14:28:40 -0400

    Initial version of a helper script that builds SCAP Workbench for OSX

---
+++ build-for-osx.sh
@@ -0,0 +1,10 @@
+mkdir -p build-osx/
+pushd build-osx/
+cmake -D SCAP_WORKBENCH_LOCAL_SCAN_ENABLED=false -D SETSID_EXECUTABLE="@executable_path/setsid" -D SCAP_WORKBENCH_SSG_DIRECTORY="@executable_path/../Resources/ssg" -D SCAP_WORKBENCH_SCAP_CONTENT_DIRECTORY="~/" ../
+make -j 4
+cp /usr/local/lib/libpcre.1.dylib ./scap-workbench.app/Contents/Frameworks/
+cp /usr/local/lib/libopenscap.8.dylib ./scap-workbench.app/Contents/Frameworks/
+echo "Copy fresh extracted SSG zip into `pwd`/scap-workbench.app/Contents/Resources/ssg/"
+echo "so that SSG README.md is at `pwd`/scap-workbench.app/Contents/Resources/ssg/README.md"
+echo "Then change directory to `pwd` and run \"sh osx-create-dmg.sh\""
+popd
