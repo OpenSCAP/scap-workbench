9864d35825bd8273e9d3499ba5c4eacfe2e2975b
Martin Preisler <mpreisle@redhat.com>
2016-07-06 14:33:03 -0400

    Minor fixes to the build-for-osx.sh script

--- build-for-osx.sh
+++ build-for-osx.sh
@@ -1,7 +1,8 @@
 mkdir -p build-osx/
 pushd build-osx/
-cmake -D SCAP_WORKBENCH_LOCAL_SCAN_ENABLED=false -D SETSID_EXECUTABLE="@executable_path/setsid" -D SCAP_WORKBENCH_SSG_DIRECTORY="@executable_path/../Resources/ssg" -D SCAP_WORKBENCH_SCAP_CONTENT_DIRECTORY="~/" ../
+cmake -D SCAP_WORKBENCH_LOCAL_SCAN_ENABLED=false -D SCAP_WORKBENCH_SSG_DIRECTORY="@executable_path/../Resources/ssg" -D SCAP_WORKBENCH_SCAP_CONTENT_DIRECTORY="~/" ../
 make -j 4
+mkdir -p ./scap-workbench.app/Contents/Frameworks/
 cp /usr/local/lib/libpcre.1.dylib ./scap-workbench.app/Contents/Frameworks/
 cp /usr/local/lib/libopenscap.8.dylib ./scap-workbench.app/Contents/Frameworks/
 echo "Copy fresh extracted SSG zip into `pwd`/scap-workbench.app/Contents/Resources/ssg/"
