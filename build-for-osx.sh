6fc822639efc7e5440d06a2033b2b8d7f3f33f25
Martin Preisler <mpreisle@redhat.com>
2016-07-06 14:44:13 -0400

    Fix permissions of copied in dylibs if necessary when building for OSX

--- build-for-osx.sh
+++ build-for-osx.sh
@@ -5,6 +5,7 @@ make -j 4
 mkdir -p ./scap-workbench.app/Contents/Frameworks/
 cp /usr/local/lib/libpcre.1.dylib ./scap-workbench.app/Contents/Frameworks/
 cp /usr/local/lib/libopenscap.8.dylib ./scap-workbench.app/Contents/Frameworks/
+chmod 755 ./scap-workbench.app/Contents/Frameworks/*.dylib
 echo "Copy fresh extracted SSG zip into `pwd`/scap-workbench.app/Contents/Resources/ssg/"
 echo "so that SSG README.md is at `pwd`/scap-workbench.app/Contents/Resources/ssg/README.md"
 echo "Then change directory to `pwd` and run \"sh osx-create-dmg.sh\""
