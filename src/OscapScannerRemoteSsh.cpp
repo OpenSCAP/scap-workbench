7f6e0ceb793a738f19cc69d79d0ccbeec215f635
Martin Preisler <mpreisle@redhat.com>
2017-09-22 12:36:20 -0400

    Port needs to be unsigned short, otherwise we run risks of overflow
    60000 is a valid port but can't be represented via just "short".

--- src/OscapScannerRemoteSsh.cpp
+++ src/OscapScannerRemoteSsh.cpp
@@ -46,7 +46,7 @@ OscapScannerRemoteSsh::OscapScannerRemoteSsh():
 OscapScannerRemoteSsh::~OscapScannerRemoteSsh()
 {}
 
-void OscapScannerRemoteSsh::splitTarget(const QString& in, QString& target, short& port)
+void OscapScannerRemoteSsh::splitTarget(const QString& in, QString& target, unsigned short& port)
 {
     // NB: We dodge a bullet here because the editor will always pass a port
     //     as the last component. A lot of checking and parsing does not need
@@ -63,7 +63,7 @@ void OscapScannerRemoteSsh::splitTarget(const QString& in, QString& target, shor
 
     {
         bool status = false;
-        const short portCandidate = portString.toShort(&status, 10);
+        const unsigned short portCandidate = portString.toUShort(&status, 10);
 
         // FIXME: Error reporting?
         port = status ? portCandidate : 22;
@@ -80,7 +80,7 @@ void OscapScannerRemoteSsh::setTarget(const QString& target)
         mSshConnection.disconnect();
 
     QString cleanTarget;
-    short port;
+    unsigned short port;
 
     splitTarget(target, cleanTarget, port);
 
