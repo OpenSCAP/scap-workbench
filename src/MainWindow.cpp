0ef261ca4fd19bc5f1e8ad25b5d0112bba9bc93b
Gabe <redhatrises@gmail.com>
2020-01-14 10:00:48 -0700

    QWidget *QDesktopWidget::screen(int) has been deprecated with no replacement

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -43,6 +43,7 @@
 #include <QFileSystemWatcher>
 #include <QDesktopWidget>
 #include <QMenu>
+#include <QScreen>
 
 #include <cassert>
 #include <set>
@@ -257,8 +258,22 @@ MainWindow::MainWindow(QWidget* parent):
 
     closeFile();
 
-    // start centered
-    move(QApplication::desktop()->screen()->rect().center() - rect().center());
+    /* start centered
+
+    As of https://codereview.qt.nokia.com/c/qt/qtbase/+/268417,
+    QWidget *QDesktopWidget::screen(int) or Application::desktop()->screen() has been
+    deprecated with no replacement. A replacement might be provided in QT6 which
+    the following 4 lines of code should be re-evaluated if a replacement is provided.
+
+    The solution of the following 4 lines of code was indirectly provided by
+    Riccardo Fagiolo in "window_main.cpp" at
+    https://stackoverflow.com/questions/46300065/dynamically-resizing-two-qlabel-implementations
+    */
+    QSize size = QGuiApplication::screens().at(0)->availableSize();
+    int x = size.width() / 2 - width() / 2;
+    int y = size.height() / 2 - height() / 2;
+    move(x, y);
+    // End stackoverflow provided code by Riccardo Fagiolo
 
     QAction* genBashRemediation = new QAction("&bash", this);
     QObject::connect(
